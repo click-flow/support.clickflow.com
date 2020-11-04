ba957acb8487216a2eecc157ccd6ca4412e2552a
erik-ekberg-sg <erik@singlegrain.com>
2020-11-04 09:24:58 -0800

    Formatting

--- services/client/.eslintrc.js
+++ services/client/.eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended',
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		"comma-dangle": ["error", "always-multiline"],
+		indent: [8, "tab"],
+	},
 }
